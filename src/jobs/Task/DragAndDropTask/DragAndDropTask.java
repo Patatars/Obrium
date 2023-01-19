60cae0b0ac469de3ca773b51df0ee3bec1b61a8a
Patatars <maxin.gubkin@mail.ru>
2023-01-19 22:45:49 +0300

    Done working border system DragAndDropTask

--- src/jobs/Task/DragAndDropTask/DragAndDropTask.java
+++ src/jobs/Task/DragAndDropTask/DragAndDropTask.java
@@ -3,13 +3,18 @@ package jobs.Task.DragAndDropTask;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Bounds;
 import javafx.scene.Cursor;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import jobs.Task.baseTask;
 
+import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -22,58 +27,73 @@ public class DragAndDropTask extends baseTask {
     private final transient Label l5;
     private final transient Label l6;
     private final transient AnchorPane a;
-    transient double oldX, oldY;
-    static double minX, maxX, minY, maxY, x, l4X;
+
 
     public DragAndDropTask() throws IOException {
         super(FXMLLoader.load(Objects.requireNonNull(DragAndDropTask.class.getResource("DragAndDropTask.fxml"))));
+
         l4 = (Label) (super.taskPane.lookup("#4"));
         l5 = (Label) (super.taskPane.lookup("#5"));
         l6 = (Label) (super.taskPane.lookup("#6"));
         a = (AnchorPane) (super.taskPane.lookup("#7"));
     }
-
+    final Bounds[] bounds = new Bounds[1];
     @Override
     public void show(Pane parent, Label pointsLabel) {
         super.show(parent, pointsLabel);
 
-        l4.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                // record a delta distance for the drag and drop operation.
-                dragDelta.x = l4.getLayoutX() - mouseEvent.getSceneX();
-                dragDelta.y = l4.getLayoutY() - mouseEvent.getSceneY();
-                l4.setCursor(Cursor.MOVE);
+        l4.setOnMousePressed(mouseEvent -> {
+            dragDelta.x = l4.getLayoutX() - mouseEvent.getSceneX();
+            dragDelta.y = l4.getLayoutY() - mouseEvent.getSceneY();
+            bounds[0] = taskPane.getBoundsInLocal();
+            System.out.println(a.getTranslateX());
+        });
+        l4.setOnMouseReleased(mouseEvent -> l4.setCursor(Cursor.HAND));
+        l4.setOnMouseDragged(mouseEvent -> {
+            moveX(l4, mouseEvent.getSceneX());
+            moveY(l4, mouseEvent.getSceneY());
+            if(Math.sqrt(Math.pow(l4.getLayoutX() - (a.getLayoutX() + a.getWidth()/2), 2) + Math.pow(l4.getLayoutY() - (a.getLayoutY()+a.getHeight()/2), 2)) <= 10){
+
+            } else {
+                l4.setText("B");
             }
         });
-        l4.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                l4.setCursor(Cursor.HAND);
-            }
-        });
-        l4.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                l4.setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
-                l4.setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
-            }
-        });
-        l4.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                l4.setCursor(Cursor.HAND);
-            }
-        });
+
     }
 
 
-    // allow the label to be dragged around.
+
+    void moveX(Label node, double cursorLocation){
+        final boolean atRightBorder = node.getLayoutX() >= (taskPane.getWidth() - node.getWidth());
+        if (atRightBorder){
+            if (cursorLocation + dragDelta.x >= node.getLayoutX()) return;
+        }
+        final boolean atLeftBorder = node.getLayoutX() <= 0;
+        if (atLeftBorder){
+            if (cursorLocation + dragDelta.x <= node.getLayoutX()) return;
+        }
+        node.setLayoutX(cursorLocation + dragDelta.x);
+    }
+
+    void moveY(Label node, double cursorLocation){
+        final boolean atBottomBorder = node.getLayoutY() >= (taskPane.getHeight() - node.getHeight());
+        if (atBottomBorder){
+            if (cursorLocation + dragDelta.y >= node.getLayoutY()) return;
+        }
+        final boolean atTopBorder = node.getLayoutY() <= 0;
+        if (atTopBorder){
+            if (cursorLocation + dragDelta.y <= node.getLayoutY()) return;
+        }
+        node.setLayoutY(cursorLocation + dragDelta.y);
+    }
+
+
+
+
     final Delta dragDelta = new Delta();
 
 
 
-    // records relative x and y co-ordinates.
     static class Delta { double x, y; }
 
     @Override
